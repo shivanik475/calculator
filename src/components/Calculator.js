a1f91efd8a7e03292c536ff968a1408b057235f8
shivanik475 <54467955+shivanik475@users.noreply.github.com>
2025-03-17 15:01:24 +0530

    Initial commit

---
+++ src/components/Calculator.js
@@ -0,0 +1,103 @@
+import React, { useState } from "react";
+
+const Calculator = () => {
+  const [data, setData] = useState("");
+  const getValue = (event) => {
+    console.log(event.target.value);
+    setData(data.concat(event.target.value));
+  };
+
+  const calculation = () => {
+    setData(eval(data).toString());
+  };
+
+  const back = () => {
+    setData(data.slice(0, data.length - 1));
+  };
+
+  const clear = () => {
+    setData("0");
+  };
+  return (
+    <div className="container">
+      <div>
+        <input placeholder="0" value={data} />
+      </div>
+      <br />
+      <div className="buttons">
+        <div>
+          <button onClick={getValue} value="(">
+            (
+          </button>
+          <button onClick={getValue} value=")">
+            )
+          </button>
+          <button onClick={getValue} value="%">
+            %
+          </button>
+          <button onClick={clear} value="AC">
+            AC
+          </button>
+        </div>
+        <div>
+          <button onClick={getValue} value="7">
+            7
+          </button>
+          <button onClick={getValue} value="8">
+            8
+          </button>
+          <button onClick={getValue} value="9">
+            9
+          </button>
+          <button onClick={getValue} value="*">
+            *
+          </button>
+        </div>
+        <div>
+          <button onClick={getValue} value="4">
+            4
+          </button>
+          <button onClick={getValue} value="5">
+            5
+          </button>
+          <button onClick={getValue} value="6">
+            6
+          </button>
+          <button onClick={getValue} value="+">
+            +
+          </button>
+        </div>
+        <div>
+          <button onClick={getValue} value="1">
+            1
+          </button>
+          <button onClick={getValue} value="2">
+            2
+          </button>
+          <button onClick={getValue} value="3">
+            3
+          </button>
+          <button onClick={getValue} value="-">
+            -
+          </button>
+        </div>
+
+        <div>
+          <button onClick={getValue} value="0">
+            0
+          </button>
+          <button onClick={back} value="X">
+            BACK
+          </button>
+          <button onClick={calculation} value="=">
+            =
+          </button>
+          <button onClick={getValue} value="/">
+            /
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+export default Calculator;
